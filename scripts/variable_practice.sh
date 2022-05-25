e27c9a374599e600dfe2c20e43e48c473e71ad6d
tawfiq01 <tawfiqul01@gmail.com>
2022-05-25 10:40:39 +0600

    Initial scripts upload

---
+++ scripts/variable_practice.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+employee="Tawfiq"
+boss="ABC"
+designation="CTO"
+echo "Hello ${employee}"
+echo "Thank you for your work, ${employee}"
+
+echo "Best wishes,"
+echo "${boss}, ${designation}"
