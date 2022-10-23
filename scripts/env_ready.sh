31ab1f210a8d4d3320e21ebf0b2fb55146175c68
tawfiq01 <tawfiqul01@gmail.com>
2022-10-23 12:56:44 +0600

    env ready script

---
+++ scripts/env_ready.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Author: Tawfiq
+#Date Created: 23-10-2022
+
+#Description
+#For ready environment for new os. 
+
+#Package Update-----------------------------------------
+sudo apt update&&sudo apt upgrade
+
+#Chrome------------------------------------------------
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+#Git Install ------------------------------------------
+#sudo apt install git
+
+# VS Code ------------------------------------------
+#sudo snap install --classic code # or code-insiders
+
+#Skypee
+#sudo snap install skype --classic
+
+#Vlc Player-------------------------------------------
+#sudo snap install vlc
+
+#Zoom------------------------------------------------ 
+#wget https://zoom.us/client/latest/zoom_amd64.deb
+#sudo apt install ./zoom_amd64.deb
+
+#Spotify
+sudo snap install spotify
+
+
